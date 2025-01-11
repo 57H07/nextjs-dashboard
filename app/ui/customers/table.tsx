5fe9ab3e30892a9e5803d5775c5e38e85447f468
Stan <sthot67@gmail.com>
2025-01-11 19:14:37 +0100

    Chapitre 15 - Fin

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,10 +1,7 @@
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 import Search from '@/app/ui/search';
-import {
-  CustomersTableType,
-  FormattedCustomersTable,
-} from '@/app/lib/definitions';
+import { FormattedCustomersTable } from '@/app/lib/definitions';
 
 export default async function CustomersTable({
   customers,
@@ -34,9 +31,9 @@ export default async function CustomersTable({
                             <Image
                               src={customer.image_url}
                               className="rounded-full"
-                              alt={`${customer.name}'s profile picture`}
                               width={28}
                               height={28}
+                              alt={`${customer.name}'s profile picture`}
                             />
                             <p>{customer.name}</p>
                           </div>
